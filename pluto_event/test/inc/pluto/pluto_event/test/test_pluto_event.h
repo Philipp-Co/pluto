d617785a45bf092aaccdafb68bff57d06c995f40
Philipp Kroll <philipp.kroll@conciso.de>
2025-05-11 21:34:47 +0200

    Umstrukturierung Code. Erstellen von Tests.

--- pluto_event/test/inc/pluto/pluto_event/test/test_pluto_event.h
+++ pluto_event/test/inc/pluto/pluto_event/test/test_pluto_event.h
@@ -22,11 +22,30 @@ void PLUTO_TEST_EventFromBufferSuccess(void);
 /// \expect The Function will return NULL.
 ///
 void PLUTO_TEST_EventFromBufferIdMissing(void);
+///
+/// \brief  Try to create a Buffer from a Buffer with unusable Containt.
+/// \assume The Buffer is unsable because of
+///             - Key/Value Pair missing
+///             - Key "event" missing
+///             - Value for "event" missing
+///             - Value < 0
+/// \expect The Function will return NULL.
+///
 void PLUTO_TEST_EventFromBufferEventMissing(void);
+///
+/// \brief  Try to create a Buffer from a Buffer with unusable Containt.
+/// \assume The Buffer is unsable because of
+///             - Key/Value Pair missing
+///             - Key "payload" missing
+///             - Value for "payload" missing
+///             - Value < 0
+/// \expect The Function will return NULL.
+///
 void PLUTO_TEST_EventFromBufferPayloadMissing(void);
 ///
 /// \brief  Dump a given Event to a Buffer.
-///
+/// \assume -
+/// \expect The given Event can be converted to a String.
 ///
 void PLUTO_TEST_EventToString(void);
 
