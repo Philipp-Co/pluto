14283c574dd8d1643b37927c03fad02a0a78a583
Philipp Kroll <philipp.kroll@conciso.de>
2026-03-13 17:30:44 +0100

    Some Stuff.

--- systemtest/run_tests.py
+++
@@ -1,6 +0,0 @@
-from unittest import TestLoader, TextTestRunner
-from subprocess import run as subprocess_run
-
-if __name__ == '__main__':
-    suite = TestLoader().discover(start_dir='tests')
-    TextTestRunner().run(suite)
