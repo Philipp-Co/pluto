2bd1e30bcb3335a98c8524aedd174fb6f3553a20
Philipp Kroll <philipp.kroll@conciso.de>
2026-03-13 17:06:57 +0100

    Some Stuff.

--- bin/send.sh
+++ bin/send.sh
@@ -6,4 +6,18 @@
 #   $1: Name of Node.
 #   $2: Payload.
 #
-curl -vvv -XPOST http://127.0.0.1:10000/runtime/events/ -d '{"id":1,"event_id":2,"timestamp":"2026-01-01T00:00:00.0+00:00","payload":"$2", "target_node_name": "$1"}'
+TARGET_NODE="$1"
+PAYLOAD="$2"
+
+echo "Start:\n"
+date -u +"%Y-%m-%dT%H:%M:%S.%N%:z"
+
+curl -vvv -X POST http://127.0.0.1:10000/runtime/events/ \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"id\": 1,
+    \"event_id\": 2,
+    \"timestamp\": \"2026-01-01T00:00:00.0+00:00\",
+    \"payload\": \"${PAYLOAD}\",
+    \"target_node_name\": \"${TARGET_NODE}\"
+  }"
--- systemtest/examplenode/src/examplenode/pluto/main.py
+++ systemtest/examplenode/src/examplenode/pluto/main.py
@@ -1,6 +1,12 @@
+from logging import getLogger, Logger
 
 
 class PlutoNode:
+    
+    def __init__(self):
+        self.__logger: Logger = getLogger(self.__class__.__name__) 
+        self.__logger.setLevel('INFO')
+        pass
 
     def setup(self, *args, **kwargs):
         print(f"--> Setup from Handler! {args}, {kwargs}")
@@ -10,9 +16,10 @@ class PlutoNode:
         print(f"Teardown from Handler! {args}, {kwargs}")
         pass
 
-    def run(self, id: int, event: int, number_of_output_queues: int, payload: str) -> str:
-        print(f'Python: Hello from run: {id}, {event}, {number_of_output_queues}, {payload}')
-        return (id, event, 0xffffffffffffffff, ('{' + f'\"python\":\"{payload}\"' + '}').encode())
+    def run(self, id: int, event: int, number_of_output_queues: int, payload: bytes) -> str:
+        self.__logger.info(f'Python: Hello from run: {id}, {event}, {number_of_output_queues}, {payload}')
+        self.__logger.info(f'Type of Payload: {type(payload)}')
+        return (id, event, 0xffffffffffffffff, payload)
 
     pass
 
